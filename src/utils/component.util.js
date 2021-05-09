b4da3ddf9e9aba58cce66c58dc38803cdc63c4e9
ruzlicali16 <ruzlicali24@gmail.com>
2021-05-09 23:29:53 +0800

    add slide animation and status bar

--- src/utils/component.util.js
+++ src/utils/component.util.js
@@ -1,5 +1,10 @@
-import { loadingController, toastController } from "@ionic/vue";
-
+import {
+  loadingController,
+  toastController,
+  createAnimation,
+} from "@ionic/vue";
+import { Plugins } from "@capacitor/core";
+const { StatusBar } = Plugins;
 class ComponentUtil {
   async popupToast(message, color, duration, position) {
     const toast = await toastController.create({
@@ -16,6 +21,35 @@ class ComponentUtil {
       message: message,
     });
     return loading;
+  }
+
+  async slideAnimation(
+    element,
+    speed,
+    positionFrom,
+    positionTo,
+    opacityFrom,
+    opacityTo
+  ) {
+    return new Promise((resolve) => {
+      const animation = createAnimation()
+        .addElement(document.querySelector(element))
+        .duration(speed)
+        .fromTo("transform", positionFrom, positionTo)
+        .fromTo("opacity", opacityFrom, opacityTo);
+      resolve(animation);
+    });
+  }
+
+  async statusBar(color) {
+    try {
+      const statusBar = await StatusBar.setBackgroundColor({
+        color: color,
+      });
+      return statusBar;
+    } catch (err) {
+      console.log(err.message);
+    }
   }
 }
 
